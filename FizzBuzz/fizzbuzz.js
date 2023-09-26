b602f016d7b95bce3081d6118ac26626f7ca7b7b
virtualprey <85876929+sammyrhymes@users.noreply.github.com>
2023-09-26 17:12:25 +0300

    Update fizzbuzz.js

--- FizzBuzz/fizzbuzz.js
+++ FizzBuzz/fizzbuzz.js
@@ -1,11 +1,15 @@
 function fizzBuzz(n){
+    
 for(let i = 1; i <= n ; i ++ ){
     if (i%3===0 && i%5===0){
         console.log('FizzBuzz');
+    
     }else if (i%3===0 && i%5!==0){
         console.log("fizz");
+    
     }else if (i%3!==0 && i%5===0){
         console.log('Buzz');
+    
     }else{
         console.log(`${i}`)
     }
